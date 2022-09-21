8c731b00f3d75ac781c64e01dcf298fb2f5f4df9
gotpop <liam@gotpop.net>
2022-09-22 00:38:23 +0100

    Page style

--- pages/project/[slug].js
+++ pages/project/[slug].js
@@ -41,7 +41,7 @@ export default function Post(props) {
                 <div className="text-gray-700 text-base mb-6">
                 {post.body && <PortableText value={post.body} />}
                 </div>
-                <a className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">View on LinkedIn</button>
+                <a className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">View on LinkedIn</a>
               </div>
               <div className="relative z-0 max-w-screen-lg shadow-md overflow-hidden lg:rounded-lg aspect-video m-6">
                 {post.imageUrl && <img src={post.imageUrl} alt="main image" />}
