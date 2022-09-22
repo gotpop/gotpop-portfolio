719149d55a64e618b90fcfaafc38c6926461e9de
gotpop <liam@gotpop.net>
2022-09-22 23:13:13 +0100

    Contact

--- pages/contact.js
+++ pages/contact.js
@@ -45,13 +45,6 @@ export default function Contact({ siteconfig }) {
   return (
     <Layout {...siteconfig}>
       <Container>
-        <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-center lg:leading-snug text-brand-primary lg:text-4xl dark:text-white">
-          Contact
-        </h1>
-        <div className="text-center">
-          <p className="text-lg">We are a here to help.</p>
-        </div>
-
         <div className="grid my-10 md:grid-cols-2">
           <div className="my-10">
             <h2 className="text-2xl font-semibold dark:text-white">
