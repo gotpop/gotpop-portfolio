8f7e831f6aa0d78f5faba650e5c6df3988e43941
gotpop <liam@gotpop.net>
2022-09-22 23:59:01 +0100

    Linked in btn

--- pages/project/[slug].js
+++ pages/project/[slug].js
@@ -1,15 +1,12 @@
+import Link from "next/link";
 import Layout from "@components/layout";
 import Container from "@components/container";
-import { useRouter } from "next/router";
-import client, {
-  getClient,
-  usePreviewSubscription,
-  PortableText
-} from "@lib/sanity";
 import ErrorPage from "next/error";
-import { singlequery, configQuery, pathquery, postquery } from "@lib/groq";
 import CategoryLabel from "@components/projects/category";
 import Pagination from "@components/pagination";
+import { useRouter } from "next/router";
+import { singlequery, configQuery, pathquery, postquery } from "@lib/groq";
+import client, { getClient, usePreviewSubscription, PortableText } from "@lib/sanity";
 
 export default function Post(props) {
   const { postdata, siteconfig, preview, projectdata } = props;
@@ -26,34 +23,11 @@ export default function Post(props) {
     initialData: projectdata,
     enabled: preview || router.query.preview !== undefined
   });
-  
+
   const { data: siteConfig } = usePreviewSubscription(configQuery, {
     initialData: siteconfig,
     enabled: preview || router.query.preview !== undefined
   });
-
-  
-  // const prevNext = (projects) => {
-  //   projects?.forEach((project, index) => {
-  //     if (post._id !== project._id) return
-  
-  //     let positionPrev
-  //     let positionNext
-      
-  //     index === 0 ? positionPrev = projects.length - 1 : positionPrev = index - 1
-  //     index === projects.length - 1 ? positionNext = 0: positionNext = index + 1
-      
-  //     const previous = projects[positionPrev].slug.current
-  //     const next = projects[positionNext].slug.current
-      
-  //     setPrevious(previous)
-  //     setNext(next)
-  //   })
-  // }
-  
-  // useEffect(() => {   
-  //   prevNext(projects)
-  // })
 
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />;
@@ -70,10 +44,17 @@ export default function Post(props) {
                 <div className="text-gray-700 text-base mb-6">
                   {post.body && <PortableText value={post.body} />}
                 </div>
-                  <div className="flex space-x-2 mt-6 mb-6">
-                    <CategoryLabel categories={post.categories} />
-                  </div>
-                <a className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">View on LinkedIn</a>
+                <div className="flex space-x-2 mt-6 mb-6">
+                  <CategoryLabel categories={post.categories} />
+                </div>
+                {post.linkedin.displaylinkedin && (
+                  <Link href={post.linkedin.href}>
+                    <a className="inline-flex items-center px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                      View on LinkedIn
+                      <span className="icon social__icon ml-4 flex"><svg width="100%" height="100%" viewBox="0 0 50 50" preserveAspectRatio="xMidYMid meet" name="linkedin"><path fill="currentColor" d="M46.3 0H3.7C1.7 0 0 1.6 0 3.6v42.8c0 2 1.7 3.6 3.7 3.6h42.6c2 0 3.7-1.6 3.7-3.6V3.6c0-2-1.7-3.6-3.7-3.6zM14.8 42.6H7.4V18.7h7.4v23.9zm-3.7-27.1c-2.4 0-4.3-1.9-4.3-4.3s1.9-4.3 4.3-4.3 4.3 1.9 4.3 4.3-1.9 4.3-4.3 4.3zm31.5 27.1h-7.4V31c0-2.8-.1-6.3-3.9-6.3-3.9 0-4.5 3-4.5 6.1v11.8h-7.4V18.7h7.1V22h.1c1-1.9 3.4-3.9 7-3.9 7.5 0 8.9 4.9 8.9 11.4v13.1z"></path></svg></span>
+                    </a>
+                  </Link>
+                )}
               </div>
               <div className="bg-white relative z-0 max-w-screen-lg shadow-md overflow-hidden lg:rounded-lg aspect-video m-6">
                 {post.imageUrl && <img src={post.imageUrl} alt="main image" />}
@@ -81,22 +62,6 @@ export default function Post(props) {
             </div>
           </Container>
           <Pagination projects={projects} post={post} />
-          {/* <Container>
-            <article className="max-w-screen-md mx-auto ">
-              <div className="flex justify-center mt-7 mb-7">
-                <Link href={`/project/${previous}`}>
-                  <a className="flex px-5 py-2 text-sm text-blue-600 rounded-full dark:text-blue-500 bg-brand-secondary/20 ">
-                  <ArrowCircleLeftIcon className="w-5 h-5 mr-2" /> Previous       
-                  </a>
-                </Link>
-                <Link href={`/project/${next}`}>
-                  <a className="flex px-5 py-2 text-sm text-blue-600 rounded-full dark:text-blue-500 bg-brand-secondary/20 ">
-                    Next <ArrowCircleRightIcon className="w-5 h-5 mr-2" />
-                  </a>
-                </Link>
-              </div>
-            </article>
-          </Container> */}
         </Layout>
       )}
     </>
@@ -109,7 +74,7 @@ export async function getStaticProps({ params, preview = false }) {
   });
 
   const project = await getClient(preview).fetch(postquery);
-  
+
   const config = await getClient(preview).fetch(configQuery);
 
   return {
