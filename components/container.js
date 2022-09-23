778e6ef9a571e33ff9bbb2c89a979073f2b5b7dc
gotpop <liam@gotpop.net>
2022-09-23 14:20:56 +0100

    Classes

--- components/container.js
+++ components/container.js
@@ -4,7 +4,7 @@ export default function Container(props) {
   return (
     <div
       className={cx(
-        "container px-8 py-5 lg:pt-8 lg:pb-14 mx-auto xl:px-5 max-w-screen-lg",
+        "container px-8 py-5 lg:py-8 mx-auto xl:px-5 max-w-screen-lg",
         props.className
       )}>
       {props.children}
