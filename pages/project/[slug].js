76ebecd7e26045797416491d7433fdfa36736843
gotpop <liam@gotpop.net>
2022-09-23 03:10:04 +0100

    Darkmode

--- pages/project/[slug].js
+++ pages/project/[slug].js
@@ -40,8 +40,8 @@ export default function Post(props) {
           <Container>
             <div className="project flex justify-center rounded-lg">
               <div className="block p-6 max-w-sm">
-                <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">{post.title}</h5>
-                <div className="text-gray-700 text-base mb-6">
+                <h5 className="text-gray-900 dark:text-gray-400 text-xl leading-tight font-medium mb-2">{post.title}</h5>
+                <div className="text-gray-700 dark:text-gray-400 text-base mb-6">
                   {post.body && <PortableText value={post.body} />}
                 </div>
                 <div className="flex my-6">
