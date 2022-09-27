9b5921367d8a2aa173ea673c19c02a7c292eaa1b
gotpop <liam@gotpop.net>
2022-09-27 02:32:45 +0100

    Cat colours

--- components/ui/label.js
+++ components/ui/label.js
@@ -2,18 +2,18 @@ import { cx } from "@utils/all";
 
 export default function Label(props) {
   const color = {
-    green: "bg-emerald-700",
-    blue: "bg-blue-600",
-    orange: "bg-orange-700",
-    purple: "bg-purple-600",
-    pink: "bg-pink-600"
+    green: "border-emerald-700",
+    blue: "border-blue-600",
+    orange: "border-orange-700",
+    purple: "border-purple-600",
+    pink: "border-pink-600"
   };
   
   return (
     <span
       className={cx(
-        "inline-flex text-xs font-medium tracking-wider uppercase text-white px-2 rounded",
-        color[props.color] || "bg-pink-600"
+        "inline-flex text-xs font-medium tracking-wider uppercase text-white px-2 rounded border-r-4 bg-slate-400",
+        color[props.color] ||  "border-pink-600"
       )}>
       {props.children}
     </span>
