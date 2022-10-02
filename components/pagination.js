aa7fa75faa7f6316c33df52a0abb87ea12426424
gotpop <liam@gotpop.net>
2022-10-02 15:57:52 +0100

    Keyboard pagination

--- components/pagination.js
+++ components/pagination.js
@@ -2,33 +2,61 @@ import Container from "@components/container";
 import Link from "next/link";
 import { ArrowCircleLeftIcon, ArrowCircleRightIcon } from "@heroicons/react/outline";
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router'
 
 export default function Pagination({ projects, post }) {
   const [next, setNext] = useState();
   const [previous, setPrevious] = useState();
+  const router = useRouter();
 
-  const prevNext = (projects) => {
-    projects?.forEach((project, index) => {
-      if (post._id !== project._id) return
+  const setPrevNext = (project, index) => {
+    if (post._id !== project._id) return
 
-      const end = projects.length - 1
-      let positionPrev
-      let positionNext
+    const end = projects.length - 1
+    let positionPrev
+    let positionNext
 
-      index === 0 ? positionPrev = end : positionPrev = index - 1
-      index === end ? positionNext = 0 : positionNext = index + 1
+    index === 0 ? positionPrev = end : positionPrev = index - 1
+    index === end ? positionNext = 0 : positionNext = index + 1
 
-      const previous = projects[positionPrev].slug.current
-      const next = projects[positionNext].slug.current
+    const previous = projects[positionPrev].slug.current
+    const next = projects[positionNext].slug.current
 
-      setPrevious(previous)
-      setNext(next)
-    })
+    setPrevious(previous)
+    setNext(next)
+  }
+
+  const prevNext = (projects, post) => {
+    if (post === undefined) return
+
+    projects?.forEach((project, index) => setPrevNext(project, index))
   }
 
   useEffect(() => {
-    prevNext(projects)
+    prevNext(projects, post)
   })
+
+  const routerPush = route => {
+    router.push({
+      pathname: `/project/${route}`
+    })
+  }
+
+  const handleKeyDown = (previous, next, e) => {
+    if (e.key !== 'ArrowLeft') {
+      routerPush(previous)
+    } else if (e.key !== 'ArrowRight') {
+      routerPush(next)
+    } else {
+      return
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown.bind(null, previous, next));
+
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [previous]);
 
   return (
     <Container>
