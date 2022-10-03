dfa2477036825c0bb18492c135f783815cbecc04
gotpop <liam@gotpop.net>
2022-10-03 16:48:01 +0100

    Debounced cb clean up

--- utils/pagination.js
+++ utils/pagination.js
@@ -29,9 +29,9 @@ export const routerPush = (router, route) => {
 
 export const handleKeyDown = (previous, next, router, e) => {
     if (e.key !== 'ArrowLeft') {
-        debounce(routerPush(router, previous), 500)
+        debounce(routerPush(router, previous), 1000)
     } else if (e.key !== 'ArrowRight') {
-        debounce(routerPush(router, next), 500)
+        debounce(routerPush(router, next), 1000)
     } else {
         return
     }
