420a4d8a9cdadcfa0bb0ad6a09effbf2e88945ac
GotPop <gotpop@users.noreply.github.com>
2023-02-20 13:41:41 +0000

    `Task:` Removing unneeded package files

--- packages/config/eslint-preset.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: ["@typescript-eslint"],
-  parser: "@typescript-eslint/parser",
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "next/core-web-vitals"],
-  rules: {
-    "no-console": ["error", { allow: ["warn", "error", "info"] }],
-  },
-}
