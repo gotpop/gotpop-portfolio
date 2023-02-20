3075ee383bcfff525850bd241e5581f8b2c771c9
GotPop <gotpop@users.noreply.github.com>
2023-02-20 13:27:04 +0000

    `Enhancement:` Removing loading components

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -15,10 +15,6 @@ const inter = Inter({
 export default function RootLayout({ children }: { children: ReactElement }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <body className={inter.className}>
         <div className="site">
